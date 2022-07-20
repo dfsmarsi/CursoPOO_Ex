1fa1e50d9bed4166f929095dabb9337ca764ff30
dfsmarsi <douglassmarsi10@gmail.com>
2022-07-19 23:18:26 -0300

    Terminado ex fixação interface e aplicado tratamento de erro

---
+++ ExFixacaoInterface/Excecoes/DomainException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ExFixacaoInterface.Excecoes
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message){}
+    }
+}
--- ExFixacaoInterface/Servicos/ContratoService.cs
+++ ExFixacaoInterface/Servicos/ContratoService.cs
@@ -1,4 +1,5 @@
 ﻿using ExFixacaoInterface.Entidades;
+using ExFixacaoInterface.Excecoes;
 using System;
 
 namespace ExFixacaoInterface.Servicos
@@ -14,7 +15,15 @@ namespace ExFixacaoInterface.Servicos
 
         public void ProcessarContrato(Contrato contrato, int meses)
         {
+            if (contrato.ValorTotal == 0)
+                throw new DomainException("Valor do contrato está zerado!");
+            if (contrato.ValorTotal < 0)
+                throw new DomainException("Valor do contrato não pode ser negativo!");
+            if (meses < 1)
+                throw new DomainException("A quantidade de parcelas tem que ser no minimo 1");
+
             double valorBase = contrato.ValorTotal / meses;
+
             for (int i = 1; i <= meses; i++)
             {
                 DateTime data = contrato.Data.AddMonths(i);
