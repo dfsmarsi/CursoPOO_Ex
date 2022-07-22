59e1676330a991099365307a454697729466b141
dfsmarsi <douglassmarsi10@gmail.com>
2022-07-22 18:23:45 -0300

    upando exercicio de hashset e equals

---
+++ ExFixacaoHashset/Curso.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace ExFixacaoHashset
+{
+    class Curso
+    {
+        public int IdAluno { get; set; }
+
+        public override int GetHashCode()
+        {
+            return IdAluno.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Curso))
+                return false;
+            Curso outro = obj as Curso;
+            return IdAluno.Equals(outro.IdAluno);
+        }
+    }
+}
---
+++ ExFixacaoHashset/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExFixacaoHashset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Curso> colecao = new HashSet<Curso>();
+            Console.Write("Quantidade alunos curso A: ");
+            int qtde = int.Parse(Console.ReadLine());
+            LerAlunosDosCursos(qtde, colecao);
+            Console.Write("Quantidade alunos curso B: ");
+            qtde = int.Parse(Console.ReadLine());
+            LerAlunosDosCursos(qtde, colecao);
+            Console.Write("Quantidade alunos curso C: ");
+            qtde = int.Parse(Console.ReadLine());
+            LerAlunosDosCursos(qtde, colecao);
+
+            Console.WriteLine("Total de alunos: " + colecao.Count);
+
+        }
+
+        private static void LerAlunosDosCursos(int qtdeAlunos, HashSet<Curso> colecao)
+        {
+            for (int i = 1; i <= qtdeAlunos; i++)
+            {
+                int id = int.Parse(Console.ReadLine());
+                colecao.Add(new Curso { IdAluno = id });
+            }
+        }
+    }
+}
